e6ce018cc2eec9f1e26e3f437f279cb3bf4eb60a
Ilvian <d.ilvian1993@gmail.com>
2023-09-04 16:36:06 -0700

    task 1 & 2

--- monty.c
+++ monty.c
@@ -69,6 +69,26 @@ void pint(stack_t **stack, unsigned int line_number)
     printf("%d\n", (*stack)->n);
 }
 
+/**
+ * pop - Removes the top element of the stack.
+ * @stack: Pointer to the stack.
+ * @line_number: Current line number in the Monty bytecode file.
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+    if (!stack || !*stack)
+    {
+        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    stack_t *temp = *stack;
+    *stack = (*stack)->next;
+    if (*stack)
+        (*stack)->prev = NULL;
+    free(temp);
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -142,6 +162,10 @@ int main(int argc, char *argv[])
         {
             pint(&stack, line_number);
         }
+	else if (strcmp(opcode, "pop") == 0)
+    {
+        pop(&stack, line_number);
+    }
         else
         {
             fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
