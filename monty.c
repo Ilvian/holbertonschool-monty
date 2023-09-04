159b3fbc58d1561f01e701e44fe470a70da22617
Ilvian <d.ilvian1993@gmail.com>
2023-09-04 15:51:52 -0700

    monty

--- monty.c
+++ monty.c
@@ -1,41 +1,136 @@
 #include "monty.h"
-#include <string.h>
 
 /**
- * main - Monty ByteCode interpreter
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
- * Return: EXIT_SUCCESS or EXIT_FAILURE
+ * push - Pushes an element onto the stack.
+ * @stack: Pointer to the stack.
+ * @line_number: Current line number in the Monty bytecode file.
+ * @value: The integer value to push onto the stack.
  */
+void push(stack_t **stack, int value)
+{
+    stack_t *new_node;
+
+    new_node = malloc(sizeof(stack_t));
+    if (!new_node)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->n = value;
+    new_node->prev = NULL;
+
+    if (*stack)
+    {
+        new_node->next = *stack;
+        (*stack)->prev = new_node;
+    }
+    else
+    {
+        new_node->next = NULL;
+    }
+
+    *stack = new_node;
+}
+
+/**
+ * pall - Prints all the values on the stack.
+ * @stack: Pointer to the stack.
+ * @line_number: Current line number in the Monty bytecode file.
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+    stack_t *current = *stack;
+
+    (void)line_number;
+
+    while (current)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
         fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
+        return (EXIT_FAILURE);
     }
 
-    char *filename = argv[1];
-    FILE *file = fopen(filename, "r");
-
+    FILE *file = fopen(argv[1], "r");
     if (!file)
     {
-        fprintf(stderr, "Error: Can't open file %s\n", filename);
-        exit(EXIT_FAILURE);
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        return (EXIT_FAILURE);
     }
 
     stack_t *stack = NULL;
     char *line = NULL;
-    size_t len = 0;
+    size_t line_len = 0;
     unsigned int line_number = 0;
 
-    while (getline(&line, &len, file) != -1)
+    while (getline(&line, &line_len, file) != -1)
     {
         line_number++;
+        char *opcode, *arg;
+        int value;
+
+        opcode = strtok(line, " \n");
+        if (!opcode || *opcode == '#')
+            continue;
+
+        if (strcmp(opcode, "push") == 0)
+        {
+            arg = strtok(NULL, " \n");
+            if (!arg)
+            {
+                fprintf(stderr, "L%d: usage: push integer\n", line_number);
+                fclose(file);
+                free(line);
+                while (stack)
+                {
+                    stack_t *temp = stack;
+                    stack = stack->next;
+                    free(temp);
+                }
+                return (EXIT_FAILURE);
+            }
+
+            value = atoi(arg);
+            if (value == 0 && *arg != '0')
+            {
+                fprintf(stderr, "L%d: usage: push integer\n", line_number);
+                fclose(file);
+                free(line);
+                while (stack)
+                {
+                    stack_t *temp = stack;
+                    stack = stack->next;
+                    free(temp);
+                }
+                return (EXIT_FAILURE);
+            }
+
+            push(&stack, value);
+        }
+        else if (strcmp(opcode, "pall") == 0)
+        {
+            pall(&stack, line_number);
+        }
     }
 
     fclose(file);
     free(line);
+
+    while (stack)
+    {
+        stack_t *temp = stack;
+        stack = stack->next;
+        free(temp);
+    }
+
     return (EXIT_SUCCESS);
 }
 
--- monty.h
+++ monty.h
@@ -1,44 +1,37 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-
+#include<stdio.h>
+#include<ctype.h>
 /**
- * struct stack_s - Doubly linked list representation of a stack
- * @n: Integer value stored in the node
- * @prev: Points to the previous element of the stack
- * @next: Points to the next element of the stack
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
-    int n;
-    struct stack_s *prev;
-    struct stack_s *next;
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
 } stack_t;
-
 /**
- * struct instruction_s - Opcode and its function
- * @opcode: The opcode
- * @f: Function to handle the opcode
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
-    char *opcode;
-    void (*f)(stack_t **stack, unsigned int line_number);
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
-void swap(stack_t **stack, unsigned int line_number);
-void add(stack_t **stack, unsigned int line_number);
-void nop(stack_t **stack, unsigned int line_number);
-stack_t *push_stack(stack_t **stack, int n);
-int pop_stack(stack_t **stack);
-void free_stack(stack_t **stack);
-int is_number(char *str);
-
 #endif
-
