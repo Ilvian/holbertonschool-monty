28933a3d971b0798335c82ef7c1c51ca72e3dcd7
Ilvian <d.ilvian1993@gmail.com>
2023-09-04 04:55:01 -0700

    monty

---
+++ monty.h
@@ -0,0 +1,10 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void push(stack_t **stack, int value);
+void pall(stack_t **stack);
+
+#endif
---
+++ monty_operations.c
@@ -0,0 +1,10 @@
+#include "monty.h"
+
+void push(stack_t **stack, int value)
+{
+}
+
+void pall(stack_t **stack)
+{
+}
+
