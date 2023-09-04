ce3e78938bf18d2122c4323b48dc3fb835ae5035
Ilvian <d.ilvian1993@gmail.com>
2023-09-04 05:04:01 -0700

    monty

--- monty_main.c
+++ monty_main.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "monty.h"
+
 int main(void)
 {
     return 0;
--- monty_operations.c
+++ monty_operations.c
@@ -1,9 +1,13 @@
 #include "monty.h"
+
 void push(stack_t **stack, int value)
 {
+    (void)stack; 
+    (void)value; 
 }
 
 void pall(stack_t **stack)
 {
+    (void)stack; 
 }
 
