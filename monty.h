45786b30856bfe2539a1172658fa48627db37db7
Ilvian <d.ilvian1993@gmail.com>
2023-09-04 14:56:32 -0700

    push

--- monty.h
+++ monty.h
@@ -34,7 +34,6 @@ typedef struct instruction_s
     void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-int is_integer(const char *str);
 void push(stack_t **stack, int value);
 void pall(stack_t **stack);
 
