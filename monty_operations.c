af58627e73d0a98692e0599cefe8f63adc862dca
Ilvian <d.ilvian1993@gmail.com>
2023-09-04 05:06:43 -0700

    monty

--- monty_operations.c
+++ monty_operations.c
@@ -1,11 +1,31 @@
 #include "monty.h"
 
 void push(stack_t **stack, int value)
-{
-    (void)stack; 
-    (void)value; 
-}
+stack_t *new_node;
 
+    /* Check if the value is an integer */
+    if (!value)
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->n = value;
+    new_node->prev = NULL;
+    new_node->next = *stack;
+
+    if (*stack != NULL)
+        (*stack)->prev = new_node;
+
+    *stack = new_node;
+}
 void pall(stack_t **stack)
 {
     (void)stack; 
