60aaccc52345401736a5f63927a9594b8c2fde6a
Arlind3553 <georgetesco@yahoo.com>
2023-09-06 01:09:24 -0700

    betty

--- monty.c
+++ monty.c
@@ -51,12 +51,12 @@ void pall(stack_t **stack, unsigned int line_number)
 */
 void pint(stack_t **stack, unsigned int line_number)
 {
-    if (!stack || !*stack)
-    {
-        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    printf("%d\n", (*stack)->n);
+	if (!stack || !*stack)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
 }
 /**
 * pop - Removes the top element of the stack.
