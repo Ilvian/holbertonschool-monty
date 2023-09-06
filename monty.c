f658bfe8eda24e8a7c7a25ef4b18b61d5d4bc6ef
Arlind3553 <georgetesco@yahoo.com>
2023-09-06 01:07:51 -0700

    betty

--- monty.c
+++ monty.c
@@ -36,13 +36,13 @@ void push(stack_t **stack, int value)
 */
 void pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *current = *stack;
-    (void)line_number;
-    while (current)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+	stack_t *current = *stack;
+	(void)line_number;
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
 }
 /**
 * pint - Prints the value at the top of the stack, followed by a new line.
