eb877b48b648e30d7da35d88bdc055be70c8fef3
Ilvian <d.ilvian1993@gmail.com>
2023-09-07 12:05:28 -0700

    betty

--- main.c
+++ main.c
@@ -9,7 +9,8 @@ int main(int argc, char *argv[])
     }
 
     FILE *file = fopen(argv[1], "r");
-    if (file == NULL)
+
+    if (!file)
     {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
         return (EXIT_FAILURE);
@@ -20,33 +21,32 @@ int main(int argc, char *argv[])
     size_t line_size = 0;
     unsigned int line_number = 0;
 
-    void (*get_op(char *token))(stack_t **stack, unsigned int line_number); // Move this here
+    void (*get_op(char *token))(stack_t **stack, unsigned int line_number);
 
     while (getline(&line, &line_size, file) != -1)
     {
         line_number++;
         char *token = strtok(line, " \t\n");
-        if (token != NULL)
+
+        if (token)
         {
             void (*opcode_func)(stack_t **, unsigned int) = get_op(token);
-            if (opcode_func != NULL)
+
+            if (opcode_func)
             {
                 opcode_func(&stack, line_number);
             }
             else
             {
                 fprintf(stderr, "L%u: unknown instruction %s\n", line_number, token);
-                free(line);
-                fclose(file);
-                free_stack(&stack); // Implement this function to free the stack
-                return (EXIT_FAILURE);
+                break;
             }
         }
     }
 
     free(line);
     fclose(file);
-    free_stack(&stack); // Implement this function to free the stack
+    free_stack(&stack);
     return (EXIT_SUCCESS);
 }
 
--- monty.c
+++ monty.c
@@ -1,58 +1,86 @@
 #include "monty.h"
 #include <stdbool.h>
-void _push(stack_t **stack, unsigned int line_number) {
-    char *arg = strtok(NULL, " \t\n");
-    if (arg) {
-        int i = 0;
-        if (arg[0] == '-') {
-            i = 1;
-        }
-        while (arg[i] != '\0') {
-            if (!isdigit(arg[i])) {
-                fprintf(stderr, "L%u: usage: push integer\n", line_number);
-                exit(EXIT_FAILURE);
-            }
-            i++;
-        }
-        int value = atoi(arg);
-        stack_t *new_node = malloc(sizeof(stack_t));
-        if (!new_node) {
-            fprintf(stderr, "Error: malloc failed\n");
-            exit(EXIT_FAILURE);
-        }
-        new_node->n = value;
-        new_node->prev = NULL;
-        new_node->next = *stack;
-        if (*stack)
-            (*stack)->prev = new_node;
-        *stack = new_node;
-    } else {
-        fprintf(stderr, "L%u: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-}
+/**
+ * _push - pushes an element onto the stack
+ * @stack: A pointer to a pointer to the top of the stack
+ * @line_number: The current line number in the script
+ */
+void _push(stack_t **stack, unsigned int line_number)
+{
+	char *arig = strtok(NULL, " \t\n");
 
+	if (arg)
+	{
+		int i = 0;
+
+		if (arg[0] == '-')
+		{
+			i = 1;
+		}
+		while (arg[i] != '\0')
+		{
+			if (!isdigit(arg[i]))
+			{
+				fprintf(stderr, "L%u: usage: push integer\n", line_number);
+				exit(EXIT_FAILURE);
+			}
+			i++;
+		}
+		int value = atoi(arg);
+		stack_t *new_node = malloc(sizeof(stack_t));
+
+		if (!new_node)
+		{
+			fprintf(stderr, "Error: malloc failed\n");
+			exit(EXIT_FAILURE);
+		}
+		new_node->n = value;
+		new_node->prev = NULL;
+		new_node->next = *stack;
+		if (*stack)
+			(*stack)->prev = new_node;
+		*stack = new_node;
+	}
+	else
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+	}
+}
+/**
+ * _pall - prints all the values on the stack, starting from the top
+ * @stack: A pointer to a pointer to the top of the stack
+ * @line_number: The current line number in the script
+ */
 void _pall(stack_t **stack, unsigned int line_number)
 {
-    (void)line_number;
-    
-    stack_t *current = *stack;
-    while (current != NULL)
-    {
-	    printf("%d\n", current->n);
-	    current = current->next;
-    }
-}
+	(void)line_number;
+	stack_t *current = *stack;
 
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
+/**
+ * free_stack - frees all the elements of a stack_t stack
+ * @stack: A pointer to a pointer to the top of the stack
+ */
 void free_stack(stack_t **stack)
 {
 	while (*stack)
 	{
 		stack_t *temp = (*stack)->next;
+
 		free(*stack);
 		*stack = temp;
 	}
 }
+/**
+ * _pint - prints the value at the top of the stack
+ * @stack: A pointer to a pointer to the top of the stack
+ * @line_number: The current line number in the script
+ */
 void _pint(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
@@ -62,6 +90,11 @@ void _pint(stack_t **stack, unsigned int line_number)
 	}
 	printf("%d\n", (*stack)->n);
 }
+/**
+ * _pop - removes the top element of the stack
+ * @stack: A pointer to a pointer to the top of the stack
+ * @line_number: The current line number in the script
+ */
 void _pop(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
@@ -73,6 +106,11 @@ void _pop(stack_t **stack, unsigned int line_number)
 	*stack = (*stack)->next;
 	free(temp);
 }
+/**
+ * _swap - swaps the top two elements of the stack
+ * @stack: A pointer to a pointer to the top of the stack
+ * @line_number: The current line number in the script
+ */
 void _swap(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
@@ -84,6 +122,11 @@ void _swap(stack_t **stack, unsigned int line_number)
 	(*stack)->n = (*stack)->next->n;
 	(*stack)->next->n = temp;
 }
+/**
+ * _add - adds the top two elements of the stack
+ * @stack: A pointer to a pointer to the top of the stack
+ * @line_number: The current line number in the script
+ */
 void _add(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
@@ -97,6 +140,11 @@ void _add(stack_t **stack, unsigned int line_number)
 	free(temp);
 	(*stack)->n = result;
 }
+/**
+ * _nop - does nothing
+ * @stack: A pointer to a pointer to the top of the stack
+ * @line_number: The current line number in the script
+ */
 void _nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
