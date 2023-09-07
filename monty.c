17f25e678a6b048353cac8bfad85b94828d3f185
Ilvian <d.ilvian1993@gmail.com>
2023-09-07 09:40:55 -0700

    task 0

--- monty.c
+++ monty.c
@@ -1,30 +1,30 @@
 #include "monty.h"
 
+#include <stdbool.h>
+
 void _push(stack_t **stack, unsigned int line_number)
 {
     char *arg = strtok(NULL, " \t\n");
 
-    if (arg && isdigit(arg[0]))
+    if (!arg || (!isdigit(arg[0]) && (arg[0] != '-' || !isdigit(arg[1]))))
     {
-        int value = atoi(arg);
-        stack_t *new_node = malloc(sizeof(stack_t));
-        if (new_node == NULL)
-        {
-            fprintf(stderr, "Error: malloc failed\n");
-            exit(EXIT_FAILURE);
-        }
-        new_node->n = value;
-        new_node->prev = NULL;
-        new_node->next = *stack;
-        if (*stack)
-            (*stack)->prev = new_node;
-        *stack = new_node;
-    }
-    else
-    {
-	    fprintf(stderr, "L%u: usage: push integer\n", line_number);
+        fprintf(stderr, "L%u: usage: push integer\n", line_number);
         exit(EXIT_FAILURE);
     }
+
+    int value = atoi(arg);
+    stack_t *new_node = malloc(sizeof(stack_t));
+    if (!new_node)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+    new_node->n = value;
+    new_node->prev = NULL;
+    new_node->next = *stack;
+    if (*stack)
+        (*stack)->prev = new_node;
+    *stack = new_node;
 }
 
 void _pall(stack_t **stack, unsigned int line_number)
