1163538ea3f0a38ef3e3d70c71137d2839c4abcf
Ilvian <d.ilvian1993@gmail.com>
2023-09-07 11:13:40 -0700

    monty

--- monty.c
+++ monty.c
@@ -1,26 +1,24 @@
 #include "monty.h"
 #include <stdbool.h>
-
 void _push(stack_t **stack, unsigned int line_number) {
     char *arg = strtok(NULL, " \t\n");
-    if (!arg || (*arg == '-' && !isdigit(arg[1])))
-    {
+    if (arg && (*arg == '-' || isdigit(*arg))) {
+        int value = atoi(arg);
+        stack_t *new_node = malloc(sizeof(stack_t));
+        if (!new_node) {
+            fprintf(stderr, "Error: malloc failed\n");
+            exit(EXIT_FAILURE);
+        }
+        new_node->n = value;
+        new_node->prev = NULL;
+        new_node->next = *stack;
+        if (*stack)
+            (*stack)->prev = new_node;
+        *stack = new_node;
+    } else {
         fprintf(stderr, "L%u: usage: push integer\n", line_number);
         exit(EXIT_FAILURE);
     }
-    int value = atoi(arg);
-    stack_t *new_node = malloc(sizeof(stack_t));
-    if (!new_node)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
-    new_node->n = value;
-    new_node->prev = NULL;
-    new_node->next = *stack;
-    if (*stack)
-        (*stack)->prev = new_node;
-    *stack = new_node;
 }
 
 void _pall(stack_t **stack, unsigned int line_number)
