a7bac185d344ef461e005cef07b6f376a3f35a6a
Ilvian <d.ilvian1993@gmail.com>
2023-09-07 09:29:56 -0700

    task 0

--- monty.c
+++ monty.c
@@ -1,40 +1,54 @@
 #include "monty.h"
 
+#include <stdbool.h>
+
+// Function to check if a string is a valid integer (positive or negative)
+bool is_valid_integer(const char *str)
+{
+    if (str == NULL || *str == '\0')
+        return false;
+
+    int i = 0;
+
+    // Handle optional leading '-' for negative numbers
+    if (str[i] == '-')
+        i++;
+
+    // Check for non-empty string with digits only
+    for (; str[i] != '\0'; i++)
+    {
+        if (!isdigit(str[i]))
+            return false; // Non-digit character found
+    }
+
+    return true; // Valid integer
+}
+
+// _push function with improved integer validation
 void _push(stack_t **stack, unsigned int line_number)
 {
     char *arg = strtok(NULL, " \t\n");
 
-    if (arg)
+    if (arg && is_valid_integer(arg))
     {
-        // Check if it's a valid integer or a negative integer
-        int is_negative = (arg[0] == '-' && isdigit(arg[1]));
-
-        if ((is_negative && is_valid_integer(arg + 1)) || (!is_negative && is_valid_integer(arg)))
+        int value = atoi(arg);
+        stack_t *new_node = malloc(sizeof(stack_t));
+        if (new_node == NULL)
         {
-            int value = atoi(arg);
-            stack_t *new_node = malloc(sizeof(stack_t));
-            if (new_node == NULL)
-            {
-                fprintf(stderr, "Error: malloc failed\n");
-                exit(EXIT_FAILURE);
-            }
-            new_node->n = value;
-            new_node->prev = NULL;
-            new_node->next = *stack;
-            if (*stack)
-                (*stack)->prev = new_node;
-            *stack = new_node;
+            fprintf(stderr, "Error: malloc failed\n");
+            exit(EXIT_FAILURE);
         }
-        else
-        {
-            fprintf(stderr, "L%u: usage: push integer\n", line_number);
-            return;
-        }
+        new_node->n = value;
+        new_node->prev = NULL;
+        new_node->next = *stack;
+        if (*stack)
+            (*stack)->prev = new_node;
+        *stack = new_node;
     }
     else
     {
         fprintf(stderr, "L%u: usage: push integer\n", line_number);
-        return;
+        exit(EXIT_FAILURE);
     }
 }
 
@@ -61,24 +75,3 @@ void free_stack(stack_t **stack)
     }
 }
 
-/* Add implementations for other opcode functions here */
-int is_valid_integer(const char *str)
-{
-    if (str == NULL || *str == '\0')
-        return 0;
-
-    int i = 0;
-
-    // Handle optional leading '-' for negative numbers
-    if (str[i] == '-')
-        i++;
-
-    // Check for non-empty string with digits only
-    for (; str[i] != '\0'; i++)
-    {
-        if (!isdigit(str[i]))
-            return 0; // Non-digit character found
-    }
-
-    return 1; // Valid integer
-}
