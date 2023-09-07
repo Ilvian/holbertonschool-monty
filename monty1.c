920a48bb1e2da0787086216549f53fa47618fac0
Ilvian <d.ilvian1993@gmail.com>
2023-09-07 10:00:43 -0700

    task 1

--- monty1.c
+++ monty1.c
@@ -6,6 +6,8 @@ void (*get_op(char *token))(stack_t **stack, unsigned int line_number)
         return (_push);
     else if (strcmp(token, "pall") == 0)
         return (_pall);
+    else if (strcmp(token, "pint") == 0)
+        return (_pint);
     return NULL; // Opcode not found
 }
 
